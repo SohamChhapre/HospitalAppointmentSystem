0700bff7394dd3f91022147d900239e21cdb59af
ashu mehra <mehraashu1017@gmail.com>
2019-12-26 15:27:52 +0530

    changes in hospital model, adding user reference

--- HospitalAppointmentSystem/hospital/models.py
+++ HospitalAppointmentSystem/hospital/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from globals.models import Logs
+from django.contrib.auth import get_user_model
 # Create your models here.
 
+User = get_user_model()
+
 class Hospital(Logs):
+    User    = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     email   = models.EmailField(null=False, blank=False,max_length=255)
     name    = models.CharField(null=False, blank=False,max_length=255)
     address = models.TextField(max_length=500)
