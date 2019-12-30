449745a0d9e0500aa8e09e3263bc1e0712f665c8
ashu mehra <mehraashu1017@gmail.com>
2019-12-30 12:20:49 +0530

    cors header included

--- HospitalAppointmentSystem/HospitalAppointmentSystem/settings.py
+++ HospitalAppointmentSystem/HospitalAppointmentSystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     # app created
     'accounts',
     'hospital',
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
